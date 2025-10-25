85f57b2ab6f3e0a050623ad95c137986da478b76
anuraghagawane <anuraghagawane10@gmail.com>
2025-10-25 19:14:13 +0530

    struct

---
+++ structs/src/main.rs
@@ -0,0 +1,41 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main() {
+    let mut user1 = build_user(String::from("anurag"), String::from("anurag"));
+
+    user1.username = String::from("ah");
+
+    let user2 = User {
+        email: String::from("ah"),
+        ..user1
+    };
+
+    println!("{}", user1.email);
+    println!("{}", user2.username);
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    println!("{}", black.0);
+
+    let Point(x, y, z) = origin;
+}
+
+fn build_user(email: String, username: String) -> User {
+    let user1 = User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1
+    };
+
+    user1
+}
