b8abbe1a1171b66a4b4c911c39ae62c9f5c297ec
anuraghagawane <anuraghagawane10@gmail.com>
2025-11-16 15:28:02 +0530

    impl design pattern

---
+++ blog/src/main.rs
@@ -0,0 +1,26 @@
+use blog::{Post, Postv2};
+
+fn main() {
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    // for postv2
+    let mut post = Postv2::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
